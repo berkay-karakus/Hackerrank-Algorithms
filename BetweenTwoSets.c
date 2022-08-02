bea6e3fc49cd4ac4f88536c7a11ff174ff605cf9
berkay-karakus <72767236+berkay-karakus@users.noreply.github.com>
2022-08-02 21:37:25 +0300

    Update BetweenTwoSets.c

--- BetweenTwoSets.c
+++ BetweenTwoSets.c
@@ -1,19 +1,19 @@
 int getTotalX(int a_count, int* a, int b_count, int* b) {
       int result = 0;
-      
-      for (int number = a[a_count-1]; number <= b[0]; number += a[a_count-1]){
+      int number;
+      for (number = a[a_count-1]; number <= b[0]; number += a[a_count-1]){
           int isContinue = 1;
           int i = 0;
           while(isContinue == 1 && i < a_count){
-              if (number % a[i] != 0){
+              if (number % a[i] != 0)
                   isContinue = 0;    
-              }
+                  i++;
           }
           i = 0;
           while(isContinue == 1 && i < b_count){
-              if (b[i] % number != 0){
+              if (b[i] % number != 0)
                     isContinue = 0;    
-              }
+                i++;
           }
           if (isContinue == 1){
               result++;   
